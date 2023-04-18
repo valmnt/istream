426284d75650a48da6b3679ba3ac925247ca2836
TANCHOUX Théo <tanchoux71@gmail.com>
2023-04-18 12:46:43 +0200

    global colors

---
+++ lib/src/resources/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xff564d4d);
+const primary = Color(0xffdb0000);
+const secondary = Color(0xffffffff);
+const tertiary = Color(0xff000000);
